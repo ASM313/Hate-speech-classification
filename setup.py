da19710345b17056f0ef7363f307bc864e9a904f
Atiq Mansoori <er_asm@outlook.com>
2024-05-28 12:45:45 +0530

    Setup and Requirements

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="hate-speech-classification",
+    version="0.0.1",
+    author="Atiq Mansoori",
+    author_email="er_asm@outlook.com",
+    packages=find_packages(),
+    install_requires=[],
+)
